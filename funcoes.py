de1b06562bc8ce12c4fcdd256ac2fad490fb7c3e
Lucas Cremasco <lucascremasco98@gmail.com>
2022-08-07 15:50:49 -0300

    07/08/2022 dmg

--- funcoes.py
+++ funcoes.py
@@ -25,3 +25,37 @@ def path_arquivos(path):
 
         img_0.append(path_n5[0])
     return path_imagens, img_0
+
+
+
+def somatorio_dimensao(img, seleciona = 'coluna'):
+    somatorio = []
+
+    if seleciona == 'coluna':
+
+        for coluna in range(y):
+            soma = np.sum(img[:,coluna])
+            somatorio.append(soma)
+        return somatorio
+        
+    else:
+        for linha in range(x):
+            soma = np.sum(img[linha,:])
+            somatorio.append(soma)
+        return somatorio
+
+def seleciona_perna(somatorio):
+    maximo = np.where(np.max(somatorio) == somatorio)[0][0]
+    somatorio2 = somatorio.copy()
+    
+    while maximo < y / 2:
+        somatorio2[maximo] = -99999
+        maximo = np.where(np.max(somatorio) == somatorio)[0][0]
+
+    interval = maximo - 150;
+    minimo = np.where(np.min(somatorio[interval:maximo]) == somatorio)[0][0]
+
+
+#    minimo_dir = np.where(np.max(somatorio[minimo] - somatorio[maximo:]))[0][0]
+
+    return minimo
